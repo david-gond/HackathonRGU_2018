42e89c2b65fdeb7802ecfe1525f5bb62610c315a
Gond David <gonddavid@gmail.com>
2018-04-14 21:54:02 +0100

    Added Database connection, Vessel class

--- src/leapmotionship/LeapMotionShip.java
+++ src/leapmotionship/LeapMotionShip.java
@@ -1,21 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package leapmotionship;
 
-/**
- *
- * @author gondd
- */
-public class LeapMotionShip {
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LeapMotionShip 
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        Semaphore semaphore = new Semaphore(0);
+        try
+        {
+            Database db = new Database(semaphore);
+            db.connect("mongodb://hackuser:hackuser@csdm-mongodb.rgu.ac.uk/hackais");
+            ArrayList<Vessel> vessels = db.getAllVessels();
+            semaphore.acquire();
+            System.out.println(vessels.size());
+            /*
+            for (Vessel vessel : vessels)
+            {
+                System.out.println(vessel);
+            }*/
+            db.disconnect();
+        }
+        catch (InterruptedException ex)
+        {
+            Logger.getLogger(LeapMotionShip.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
 }
